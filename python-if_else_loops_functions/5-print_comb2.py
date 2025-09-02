39355557c3dc1ae81225ca952e6acb5c563121a3
root <root@DESKTOP-PLCA8BP.localdomain>
2025-09-02 20:55:11 +0200

    a

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,8 +3,6 @@ i = 0
 while i <= 99:
     if i < 10:
         print("0{}, ".format(i), end="")
-    elif i < 99:
+    else:
         print("{}, ".format(i), end="")
-    elif i == 99:
-        print("{}".format(i))
     i+= 1
