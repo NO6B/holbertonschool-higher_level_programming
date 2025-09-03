50a4e5bbe0debe840c67560bebfb7ea53c7887d4
abdel <abdel_azrhar@hotmail.com>
2025-09-03 22:40:53 +0200

    a

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    j = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            j += chr(ord(i)-32)
+        else:
+            j += i
+    return(j)
