ff776913e04a82066deddd03f20914973c28cf16
Abdel <abdel_azrhar@hotmail.com>
2025-09-05 10:58:13 +0200

    a

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+import sys
+if __name__ == "__main__":
+
+    arguments = sys.argv[1:]
+    if len(arguments) == 1:
+        argm = "argument"
+    elif len(arguments) > 1:
+        argm = "arguments"
+    elif len(arguments) < 1:
+        argm = "."
+
+    print(f"{len(arguments)} {argm}:")
+    
+    for j, i in enumerate(arguments, start= 1):
+        print(f"{j}: {i}")
