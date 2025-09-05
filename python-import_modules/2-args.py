964c9c2af2ed85fd368c9e11a5cd495dcd91e57c
Abdel <abdel_azrhar@hotmail.com>
2025-09-05 11:28:00 +0200

    a

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,14 +3,14 @@ import sys
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
-    argm = len(arguments)
+    argv = len(arguments)
 
-    if argm == 0:
+    if argv == 0:
         print("0 arguments.")
-    elif argm == 1:
+    elif argv == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(argm))
+        print("{} arguments:".format(argv))
 
     for i, j in enumerate(arguments, start=1):
         print("{}: {}".format(i, j))
