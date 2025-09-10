a51609c4c2d88418c20a913059d24c564b06bd3f
aBDEL <abdel_azrhar@hotmail.com>
2025-09-10 15:03:54 +0200

    a

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_value = max(a_dictionary)
+    max_value = max(a_dictionary, key=a_dictionary.get)
     return max_value
