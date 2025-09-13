50888423c515724870a3e7136331413f88c0f970
Abdel <abdel_azrhar@hotmail.com>
2025-09-13 12:59:32 +0200

    a

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for i in text:
+        print(i, end="")
+        if i in ".?:":
+            print("\n")
+            print("\n")
