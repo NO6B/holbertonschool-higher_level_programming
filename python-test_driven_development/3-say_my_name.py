7bfe802f6b197923458cd37c6cc91905b15a2616
aBDEL <abdel_azrhar@hotmail.com>
2025-09-13 13:49:05 +0200

    a

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-function that prints 
+function that prints
 """
 
 
 def say_my_name(first_name, last_name=""):
     """function that prints My name is
-    
+
     args:
         <first name: (str): The first name
         <last name: (str): The last name
-    
+
     Raises:
         TypeError: If first_name or last_name is not a string.
-    
-    
+
+
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
