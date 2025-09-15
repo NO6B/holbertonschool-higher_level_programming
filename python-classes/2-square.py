c461782d4332aef9d95aaacc6e55d518e44ffe70
aBDEL <abdel_azrhar@hotmail.com>
2025-09-15 13:15:11 +0200

    a

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,4 +6,7 @@ class Square:
     """square class with a private size"""
     def __init__(self, size = 0):
         self.__size = size
-
+        if not isinstance(size, (int)):
+            raise TypeError("a must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
