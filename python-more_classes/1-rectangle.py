cd27bbc68b858511d5e70f6fcfe97c54148dfca5
aBDEL <abdel_azrhar@hotmail.com>
2025-09-17 11:53:22 +0200

    fix private  instance attribute

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """empty rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -18,7 +18,7 @@ class Rectangle:
         """set value of width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -32,6 +32,6 @@ class Rectangle:
         """set value of height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
