d95a91b913bbfe0e66915ac962a816568fdd872f
aBDEL <abdel_azrhar@hotmail.com>
2025-09-22 11:39:55 +0200

    a

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class MyList that inherits from list"""
+
+
+class MyList(list):
+    """class MyList that inherits from list """
+    def print_sorted(self):
+        """rint the list in ascending order"""
+        print(sorted(list))
