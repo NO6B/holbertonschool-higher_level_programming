a5963d10b1b164d610208316198e8a054da77224
aBDEL <abdel_azrhar@hotmail.com>
2025-09-22 15:24:41 +0200

    a

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""BaseGeomery Class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        """method that raises an Exception with the message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
