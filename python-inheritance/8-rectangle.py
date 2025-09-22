adbf6e4792c63f5f2f854acf27cbc1f2072a6332
aBDEL <abdel_azrhar@hotmail.com>
2025-09-22 16:11:06 +0200

    a

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """BaseGeomery Class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     """empty class"""
     def __init__(self, width, height):
         self.__width = width
