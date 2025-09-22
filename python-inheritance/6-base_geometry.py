e0760b059cae4d82a79be59275601fc407499049
aBDEL <abdel_azrhar@hotmail.com>
2025-09-22 15:20:23 +0200

    a

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""BaseGeomery Class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        """method that raises an Exception with the message"""
+        raise Exception("area() is not implemented")
