e34e061fcac3dfed0315b980f0bd505bc3dd2975
aBDEL <abdel_azrhar@hotmail.com>
2025-09-22 16:55:46 +0200

    a

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -37,6 +37,7 @@ class Rectangle(BaseGeometry):
 
 
 class Square(Rectangle):
+    """class Rectangle that inherits from BaseGeometry"""
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
