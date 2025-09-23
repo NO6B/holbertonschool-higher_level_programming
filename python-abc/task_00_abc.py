88cc24583ce11cc4924413749b2c2043dd34ff7a
aBDEL <abdel_azrhar@hotmail.com>
2025-09-23 16:09:23 +0200

    a

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Animal Class"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def Sound(self):
+        pass
+
+
+class Dog(Animal):
+    """dog class"""
+    def Sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """cat class"""
+    def sound(self):
+        return "Meow"
