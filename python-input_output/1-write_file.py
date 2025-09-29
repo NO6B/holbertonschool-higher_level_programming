79b23ed6a57e1f5c38ba1e2232566ac29b23e062
aBDEL <abdel_azrhar@hotmail.com>
2025-09-29 11:18:05 +0200

    pycode fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""write_file function"""
+
+
 def write_file(filename="", text=""):
+    """function that writes a string to a text file"""
     count = 0
     with open(filename, mode="w", encoding="utf-8") as f:
         for i in text:
