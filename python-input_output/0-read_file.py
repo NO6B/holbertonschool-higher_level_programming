254518d99a5c769a4ec020881e8e1bb20d212946
aBDEL <abdel_azrhar@hotmail.com>
2025-09-29 10:41:38 +0200

    a

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""read_file function"""
+
+
 def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
