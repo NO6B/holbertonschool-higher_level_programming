5d545845049a4ff9c7e0205dc4f8b187298018bf
Abdel <abdel_azrhar@hotmail.com>
2025-10-02 10:58:59 +0200

    serialize and deserialize functions

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pickle
+"""2 function for serialize and deserialize"""
+
+
+def serialize_and_save_to_file(data, filename):
+    """serialize_and_save_to_file"""
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """load_and_deserialize"""
+    with open(filename, "wb") as f:
+        return pickle.loads(filename)
