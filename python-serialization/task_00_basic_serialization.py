327c4e284f00d8e30a3604a8dd55bab266669eea
Abdel <abdel_azrhar@hotmail.com>
2025-10-02 12:56:36 +0200

    fix read and write

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,11 +5,11 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     """serialize_and_save_to_file"""
-    with open(filename, "wb") as f:
+    with open(filename, "w") as f:
         json.dump(data, f)
 
 
 def load_and_deserialize(filename):
     """load_and_deserialize"""
-    with open(filename, "rb") as f:
+    with open(filename, "r") as f:
         return json.load(f)
