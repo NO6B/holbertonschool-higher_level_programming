a9bb8ae43729dd0f06b462db2430e6bfa9a75bac
aBDEL <abdel_azrhar@hotmail.com>
2025-10-02 17:54:50 +0200

    add pycode style

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+"""convert_csv_to_json function"""
 import csv
 import json
 
 
-def convert_csv_to_json():
-    pass
+def convert_csv_to_json(csv_filename):
+    """function convert to json"""
+    try:
+        with open(csv_filename, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            data = list(reader)
+
+        with open("data.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
+
+        return True
+    except FileNotFoundError:
+        return False
