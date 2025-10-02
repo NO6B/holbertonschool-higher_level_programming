ed1c21b199ed219be15fb604722c5df49685094f
Abdel <abdel_azrhar@hotmail.com>
2025-10-02 12:55:25 +0200

    change pikle to json

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """2 function for serialize and deserialize"""
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """serialize_and_save_to_file"""
     with open(filename, "wb") as f:
-        pickle.dump(data, f)
+        json.dump(data, f)
 
 
 def load_and_deserialize(filename):
     """load_and_deserialize"""
     with open(filename, "rb") as f:
-        return pickle.load(f)
+        return json.load(f)
