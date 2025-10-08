032a2fdb51a40df73cbac079c55fdeed4225c9d5
aBDEL <abdel_azrhar@hotmail.com>
2025-10-08 20:11:50 +0200

    update task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,34 @@
+import requests
+
+import csv
+
+
+def fetch_and_print_posts():
+    reponse = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("Status Code:", reponse.status_code)
+    if reponse.status_code == 200:
+        posts = reponse.json()
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+
+        csv_post = []
+        for post in posts:
+            csv_post.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+
+        with open("posts.csv", "w", newline='', encoding='utf-8') as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(csv_post)
