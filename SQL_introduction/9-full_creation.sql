635ec0351c95cfa1478c23106ad19fec31ff1346
aBDEL <abdel_azrhar@hotmail.com>
2025-10-21 19:51:32 +0200

    change " by '

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS second_table(
 INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
-(3, "BOB", 14),
-(4, "George", 8);
+(3, 'BOB', 14),
+(4, 'George', 8);
