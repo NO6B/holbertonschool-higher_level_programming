d8e036cc9813941c88c8d68114c67fe0414988c5
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2025-11-06 14:56:44 +0100

    task print argv if found

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+// Les arguments réels commencent à l’index 2
+if (argv.length > 2) {
+  console.log("Argument found");
+} else {
+  console.log("No argument");
+}
