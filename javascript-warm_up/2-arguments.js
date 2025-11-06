2c96b29847ee3915e71928e8713c1db84682ab64
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2025-11-06 15:00:26 +0100

    fix com

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,6 +5,7 @@ const { argv } = require('node:process');
 // Les arguments réels commencent à l’index 2
 if (argv.length > 2) {
   console.log('Argument found');
+  // si non argv
 } else {
   console.log('No argument');
 }
